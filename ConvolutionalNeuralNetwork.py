25fff0f2d63e60e77578e475ccee88ddbc5539ef
fastslash8 <fastslash8@gmail.com>
2017-02-09 19:06:51 -0600

    Fixed more bugs

--- ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork.py
@@ -11,6 +11,7 @@ import mlayers as ml
 from scipy import misc
 
 EPOCHS = 10
+LEARN_RATE = 0.001
 
 class ConvolutionalLayer():
     cache = np.array([0])  #Used to store the values for back-propagation
@@ -41,6 +42,8 @@ class ConvolutionalLayer():
     def forward(self, inputArr):
         #filters = list of all filters
         #outputs = list(?) of outputs
+        self.cache = inputArr
+
         self.o_width = int((self.width - self.fsize)/self.stride) + 1
         self.o_height = int((self.height - self.fsize)/self.stride) + 1
 
@@ -83,18 +86,20 @@ class ConvolutionalLayer():
                         #iteration TODO
                         for m in range(self.o_height):
                             for n in range(self.o_width):
-                                dCdf[i][j] += self.cache[i + m*self.stride][j + n*self.stride][layer] * gradient[m][n][f]
-                                self.bias[f] += gradient[m][n][f]
+                                print(i,j,m,n)
+                                print(np.shape(gradient))
+                                dCdf[i][j] += self.cache[i + m*self.stride][j + n*self.stride][layer] * gradient[f][m][n]
+                                self.bias[f] += gradient[f][m][n]
 
                                 #Rotating filter for convolution
-                                dCdx[m][n][layer] += self.filters[f][layer][-i][-j] * gradient[m][n][f]
+                                dCdx[m][n][layer] += self.filters[f][layer][-i][-j] * gradient[f][m][n]
 
-                self.filters[f][layer] += dzdf
-
-
+                self.filters[f][layer] += dCdf
 
 
-        return np.dot(dCdx, gradient)
+
+
+        return dCdx#np.dot(dCdx, gradient)
 
 
 class MaxPoolingLayer():
@@ -169,9 +174,9 @@ class FullyConnectedLayer():
         self.old_height = input_height
         self.old_width = input_width
 
-        self.rows = input_height * input_width * input_depth
-        self.cols = new_dim
-        #self.depth = input_depth
+        self.cols = input_height * input_width * input_depth
+        self.rows = new_dim
+        self.depth = input_depth
 
         self.cache = np.zeros((self.rows,1))
         self.weights = np.random.uniform(-np.sqrt(1./self.cols), np.sqrt(1./self.cols), (self.rows, self.cols+1))
@@ -179,7 +184,7 @@ class FullyConnectedLayer():
         self.mem_weights = np.zeros(self.weights.shape)
     def forward(self, inputArr):
         flatArr = np.ndarray.flatten(inputArr)
-        print(np.shape(inputArr), self.rows)
+        #print(np.shape(inputArr), self.rows)
 
         self.cache = np.resize(np.append(flatArr, [1]), (len(flatArr) + 1, 1))
         self.mem_weights = 0.9*self.mem_weights + 0.1*(self.weights ** 2) #incrementing for adagrad
@@ -189,11 +194,11 @@ class FullyConnectedLayer():
     def backward(self, gradient):
         self.weights -= np.outer(gradient, self.cache.T) * LEARN_RATE / np.sqrt(self.mem_weights + 1e-8)
 
-        return np.reshape(np.dot(self.weights.T, gradient)[:len(np.dot(self.weights.T, gradient)) - 1], (self.old_height, self.old_width))
+        return np.reshape(np.dot(self.weights.T, gradient)[:len(np.dot(self.weights.T, gradient)) - 1], (self.depth, self.old_height, self.old_width))
 
 possible_classifications = 2
 
-layers = [ConvolutionalLayer(16,16,1,2,3,2,0), ReLULayer(), FullyConnectedLayer(2,7,7,9), ml.InnerLayer(10,possible_classifications), ml.SoftmaxLayer()]
+layers = [ConvolutionalLayer(16,16,1,2,3,2,0), ReLULayer(), FullyConnectedLayer(2,7,7,10), ml.InnerLayer(possible_classifications, 10), ml.SoftmaxLayer()]
 
 
 for i in range(EPOCHS):
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from scipy import misc
 import numpy as np
 
-arr = np.array([[-1,2,3,4,5,6],[4,5,6,7,8,9],[7,8,9,1,2,3]])
+arr = np.array([[1,2,3],[4,5,6]], [[11,22,33], [44,55,66]])
 
-print(np.maximum(arr, 0))
+arr = np.reshape(np.ndarray.flatten(arr), (2,4,3))
+print(arr)
